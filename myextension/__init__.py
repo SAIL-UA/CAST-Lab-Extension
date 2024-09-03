62bfac6ce282fde86ff5124322c066f0c5bba7f9
Emily Jackson <emilyjjackson1123@gmail.com>
2024-09-03 16:43:17 -0500

    Logging extension working

--- myextension/__init__.py
+++ myextension/__init__.py
@@ -1,5 +1,5 @@
 try:
-    from ._version import __version__
+    from myextension._version import __version__
 except ImportError:
     # Fallback when using the package in dev mode without installing
     # in editable mode with pip. It is highly recommended to install
@@ -7,10 +7,18 @@ except ImportError:
     import warnings
     warnings.warn("Importing 'myextension' outside a proper installation.")
     __version__ = "dev"
-
+    
+from myextension.handler import setup_handlers
 
 def _jupyter_labextension_paths():
     return [{
-        "src": "labextension",
-        "dest": "myextension"
+        "src": "labextension",  # Directory containing the built assets
+        "dest": "myextension"   # Name of the JupyterLab extension
     }]
+
+def _jupyter_server_extension_points():
+    return [{
+        'module': 'myextension',
+    }]
+def load_jupyter_server_extension(nb_server_app):
+    setup_handlers(nb_server_app.web_app)
--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
-__import__("setuptools").setup()
+from setuptools import setup, find_packages
+
+setup(
+    name='myextension',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'jupyterlab',
+    ],
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'myextension = myextension:setup_handlers',
+        ]
+    },
+)
